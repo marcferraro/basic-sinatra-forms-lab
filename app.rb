3e06fa8032599e9797e95a176855dae8b7e036c7
Marc <marc.ferraro1@gmail.com>
2021-01-11 22:21:04 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/team' do
+        @team = params.values
+        erb :team
+    end
 
 end
